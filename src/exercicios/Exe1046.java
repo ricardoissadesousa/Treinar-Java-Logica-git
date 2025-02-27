67bda7d2dd72244c82a2d69cf80ef75c5351758d
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-02-26 21:07:57 -0300

    exercicio 1046 do uri

---
+++ src/exercicios/Exe1046.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exe1046 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int horaInicio, horaFim;
+
+        int horaInicial = sc.nextInt();
+        int horaFinal = sc.nextInt();
+
+        int duracao;
+        if (horaInicial < horaFinal) {
+            duracao = horaFinal - horaInicial;
+        }
+        else {
+            duracao = 24 - horaInicial + horaFinal;
+        }
+
+        System.out.println("O JOGO DUROU " + duracao + " HORA(S)");
+
+
+
+
+    }
+}
